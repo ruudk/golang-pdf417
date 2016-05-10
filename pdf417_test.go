7118ea14f52c4d40645e99a623ec870aa2e40015
Ruud Kamphuis <ruudk@mphuis.com>
2016-05-10 21:51:26 +0200

    Added PDF417 encoder

---
+++ pdf417_test.go
@@ -0,0 +1,47 @@
+package pdf417
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEncode2(t *testing.T) {
+	assert := assert.New(t)
+
+	barcode := Encode("Ruud")
+
+	assert.Equal(barcode.Data, "Ruud")
+	assert.Equal(barcode.Columns, DEFAULT_COLUMNS)
+	assert.Equal(barcode.Rows, 2)
+	assert.Equal(barcode.SecurityLevel, DEFAULT_SECURITY_LEVEL)
+	assert.Equal(barcode.CodeWords, []int{4, 537, 620, 119, 266, 457, 253, 518, 74, 589, 901, 37})
+	assert.Equal(
+		[][]int{
+			[]int{
+				130728,
+				120256,
+				125560,
+				78210,
+				116376,
+				106894,
+				74544,
+				117894,
+				128318,
+				260649,
+			},
+			[]int{
+				130728,
+				129678,
+				104332,
+				123494,
+				125192,
+				115830,
+				114524,
+				120588,
+				128352,
+				260649,
+			},
+		},
+		barcode.Codes,
+	);
+}
